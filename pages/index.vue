059b6f5a075b0c70ec32a13b6ac2b2585b98f76a
aocattleya <aocattleya@yahoo.co.jp>
2020-02-10 02:42:07 +0900

    add: ローディングアニメーションを追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,44 @@
 <template>
-  <div :class="{ hinge: bom }" class="container animated">
-    <div v-if="isActive === false" class="profiles">
-      <SignBoard v-on:click.native="toggleSwitch()" v-if="bomCount <= 9" />
-      <Profile @bom-event="bomLargeFrame" />
+  <div>
+    <div id="loading" v-show="loading">
+      <img src="~/assets/images/loading.gif" alt="loading" />
     </div>
-    <transition appear>
-      <div v-if="isActive === true" class="projects-content">
-        <Achievement @profile-open="profileOpen" />
+    <div id="contents" v-show="!loading">
+      <div :class="{ hinge: bom }" class="container animated">
+        <div v-if="isActive === false" class="profiles">
+          <SignBoard v-on:click.native="toggleSwitch()" v-if="bomCount <= 9" />
+          <Profile @bom-event="bomLargeFrame" />
+        </div>
+        <transition appear>
+          <div v-if="isActive === true" class="projects-content">
+            <Achievement @profile-open="profileOpen" />
+          </div>
+        </transition>
       </div>
-    </transition>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
 @import '~assets/style/_mixins.scss';
+
+#loading {
+  width: 100%;
+  height: 100vh;
+  background-color: #fff;
+  position: relative;
+}
+
+#loading img {
+  position: absolute;
+  width: 250px;
+  height: 250px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border: dashed 4px #283c5f;
+  border-radius: 50%;
+}
 
 .container {
   margin: 0 auto;
@@ -39,7 +64,6 @@
   top: calc(50% - 45%);
   padding-top: 40px;
   padding-bottom: 40px;
-  border: solid 5px #283c5f;
   margin: 0 auto;
   background-color: white;
 }
@@ -49,6 +73,7 @@
 import Profile from '@/components/Profile.vue'
 import Achievement from '@/components/Achievement.vue'
 import SignBoard from '@/components/SignBoard.vue'
+
 export default {
   components: {
     Profile,
@@ -57,11 +82,17 @@ export default {
   },
   data() {
     return {
+      loading: true,
       bomCount: null,
       bom: false,
       signboardMsg: '作った物&実績',
       isActive: false
     }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.loading = false
+    }, 2000)
   },
   methods: {
     toggleSwitch() {
