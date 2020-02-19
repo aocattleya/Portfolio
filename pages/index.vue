07cb00f06a3d17ed06cbd9f79189c10676c763f4
aocattleya <aocattleya@yahoo.co.jp>
2020-02-20 02:22:18 +0900

    remove: ローディングアニメーションの削除

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,21 @@
 <template>
   <div>
-    <div id="loading" v-show="loading">
-      <img src="~/assets/images/loading.gif" alt="loading" />
-    </div>
-    <div id="contents" v-show="!loading">
-      <div :class="{ hinge: bom }" class="container animated">
-        <div v-if="isActive === false" class="profiles">
-          <SignBoard v-on:click.native="toggleSwitch()" v-if="bomCount <= 9" />
-          <Profile @bom-event="bomLargeFrame" />
-        </div>
-        <transition appear>
-          <div v-if="isActive === true" class="projects-content">
-            <Achievement @profile-open="profileOpen" />
-          </div>
-        </transition>
+    <div :class="{ hinge: bom }" class="container animated">
+      <div v-if="isActive === false" class="profiles">
+        <SignBoard v-on:click.native="toggleSwitch()" v-if="bomCount <= 9" />
+        <Profile @bom-event="bomLargeFrame" />
       </div>
+      <transition appear>
+        <div v-if="isActive === true" class="projects-content">
+          <Achievement @profile-open="profileOpen" />
+        </div>
+      </transition>
     </div>
   </div>
 </template>
 
 <style lang="scss">
 @import '~assets/style/_mixins.scss';
-
-#loading {
-  width: 100%;
-  height: 100vh;
-  background-color: #fff;
-  position: relative;
-}
-
-#loading img {
-  position: absolute;
-  width: 250px;
-  height: 250px;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  border: dashed 4px #283c5f;
-  border-radius: 50%;
-}
 
 .container {
   margin: 0 auto;
@@ -82,17 +59,11 @@ export default {
   },
   data() {
     return {
-      loading: true,
       bomCount: null,
       bom: false,
       signboardMsg: '作った物&実績',
       isActive: false
     }
-  },
-  mounted() {
-    setTimeout(() => {
-      this.loading = false
-    }, 2500)
   },
   methods: {
     toggleSwitch() {
